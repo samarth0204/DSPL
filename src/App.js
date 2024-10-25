cd55136f48d7cf79dbc2f02504ea75283dab168a
samrth0204 <samarth0204@gmail.com>
2024-10-25 22:07:07 +0530

    routes updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from './pages/home';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AllProducts from './pages/allProducts';
 import AboutUsAll from './pages/aboutUsAll';
 import ContactUs from './pages/contactUs';
@@ -9,13 +9,15 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/all-products" element={<AllProducts />} />
-        <Route path="/about" element={<AboutUsAll />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/all-products" element={<AllProducts />} />
+          <Route path="/about" element={<AboutUsAll />} />
+          <Route path="/contact" element={<ContactUs />} />
+          {/* Catch-all route to redirect to Home */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
