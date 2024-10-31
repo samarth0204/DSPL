40a4549dcfee7a84d13b07eab3372649f3e6cfbb
samrth0204 <samarth0204@gmail.com>
2024-10-31 09:02:41 +0530

    products added

--- src/constants/productInfo.js
+++ src/constants/productInfo.js
@@ -13,19 +13,9 @@ import SambharM from "../images/Sambhar Masala.png";
 import turmeric from "../images/Turmeric.png";
 
 export const products = [
+  
   { 
     id: 1, 
-    name: 'Garam Masala', 
-    category: 'Blended Spices', 
-    image: garamM,
-    description: 'A select blend of 13 spices go into this grand old universal taste enhancer. Being chilli based, it provides an exotic red gravy to dishes.',
-    usage: 'It is used essentially for preparing vegetarian dishes requiring a gravy. Gujaratis and Marwaris normally use it in lentils (dal) and for filling in snacks such as samosa, usal, patra, farsan etc.',
-    packaging: ' 25 gms / 50 gms / 100 gms / 200 gms',
-    extraInfo: 'It is widely used all over India on account of its being a less pungent garam masala. Fennel, Tejpatta (Cinnamon leaves) and Trifala imparts a cooling effect to this blend.',
-    color: '#FF6B6B'
-  },
-  { 
-    id: 2, 
     name: 'Turmeric Powder', 
     category: 'Pure Spices', 
     image: turmeric,
@@ -36,7 +26,7 @@ export const products = [
     color: '#FFD93D'
   },
   { 
-    id: 3, 
+    id: 2, 
     name: 'Coriander Powder', 
     category: 'Pure Spices', 
     image: coriander,
@@ -47,7 +37,7 @@ export const products = [
     color: '#6BCB77'
   },
   { 
-    id: 4, 
+    id: 3, 
     name: 'Red Chilli ', 
     category: 'Pure Spices', 
     image: redChilli,
@@ -56,6 +46,17 @@ export const products = [
     packaging: '10 Rs / 100 gms / 200 gms / 500 gms',
     extraInfo: 'Known for its digestive benefits and rich iron content.',
     color: '#4D96FF'
+  },
+  { 
+    id: 4, 
+    name: 'Garam Masala', 
+    category: 'Blended Spices', 
+    image: garamM,
+    description: 'A select blend of 13 spices go into this grand old universal taste enhancer. Being chilli based, it provides an exotic red gravy to dishes.',
+    usage: 'It is used essentially for preparing vegetarian dishes requiring a gravy. Gujaratis and Marwaris normally use it in lentils (dal) and for filling in snacks such as samosa, usal, patra, farsan etc.',
+    packaging: ' 25 gms / 50 gms / 100 gms / 200 gms',
+    extraInfo: 'It is widely used all over India on account of its being a less pungent garam masala. Fennel, Tejpatta (Cinnamon leaves) and Trifala imparts a cooling effect to this blend.',
+    color: '#FF6B6B'
   },
   { 
     id: 5, 
